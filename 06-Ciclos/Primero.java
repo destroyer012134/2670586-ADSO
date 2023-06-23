58a02d9883c982a3d891480ddc5761725d081afb
destroyer012134 <miguelangelmorasena@gmail.com>
2023-06-23 13:07:23 -0500

    01

---
+++ 06-Ciclos/Primero.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Primero {
+    public static void main (String[] args){
+
+        Scanner teclado = new Scanner (System.in);
+
+
+        
+
+        System.out.println("Ingresar cantidad de notas: ");
+        int cantidad = teclado.nextInt();
+
+        double sumarNotas = 0;
+        int contador = 1;
+
+        while(contador <= cantidad){
+            System.out.println("=> Ingrese nota"+contador+": ");
+            double nota = teclado.nextDouble();
+
+            sumarNotas = sumarNotas + nota;
+
+            contador = contador + 1;
+        }
+
+        double promedio = sumaNotas / cantidad;
+        System.out.println("=> El promedio del estudiante es: "+promedio);
+
+    }
+}
---
+++ 06-Ciclos/Segundo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Segundo {
+    public static void main (String[] args){
+
+        Scanner teclado = new Scanner (System.in);
+
+        System.out.println("=> Ingresar cantidad de Materias");
+        int cantidad = teclado.nextInt();
+
+        double sumarNotas = 0, sumarCreditos = 0;
+        int contador = 1;
+
+        while(contador <= cantidad){
+            System.out.println("=> ingrese Nota Materia "+contador+" : ");
+            Float nota = teclado.nextFloat();
+
+            System.out.println("=> Ingrese Credios Materia "+contador+" : ");
+            int creditos = teclado.nextInt();
+
+            sumarNotas = sumarNotas + (nota*creditos);
+            sumarCreditos = sumarCreditos + creditos;
+            double prom = sumarNotas / cantidad / sumarCreditos;
+            contador = contador + 1;
+
+        }
+
+        System.out.println("Total creditos: "+sumarCreditos);
+        System.out.println("El promedio del semestre es: "+prom);
+
+
+    }
+}
