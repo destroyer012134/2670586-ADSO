747ed87d35e91a64181de23fde1774c0f031fcd9
Miguel Angel Mora Galeano <miguelangelmorasena@gmail.com>
2023-07-21 13:05:38 -0500

    02

---
+++ 06-Ciclos/Cuarto.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class cuarto {
+    public static void main (String[] args){
+
+        teclado = new Scanner (System.in);
+
+        System.out.printl("--------------BIENVENIDO-----------------------");
+        System.out.printl("| 1.Docente                                   |");
+        System.out.printl("| 2.Estudainte                                |");
+        System.out.printl("| 3.Salir                                     |");
+        System.out.printl("-----------------------------------------------")
+        int opcion = teclado.nextInt();
+        
+    }
+}
--- 06-Ciclos/Tercero.java
+++ 06-Ciclos/Tercero.java
@@ -5,6 +5,27 @@ public class Tercero {
 
         Scanner teclado = new Scanner (System.in);
 
-        
+        System.out.println("=> Ingrese la altura de una piramide");
+        int altura = teclado.nextInt();
+
+        int i = 1;
+        while (i <= altura) {
+            // Imprimir espacios en blanco
+            int espacios = altura - i;
+            while (espacios > 0) {
+                System.out.print(" ");
+                espacios--;
+            }
+            
+            // Imprimir caracteres
+            int caracteres = 2 * i - 1;
+            while (caracteres > 0) {
+                System.out.print("x");
+                caracteres--;
+            }
+            
+            System.out.println(); // Cambiar de línea para la siguiente fila de la pirámide
+            i++;
+        }
     }
 }
